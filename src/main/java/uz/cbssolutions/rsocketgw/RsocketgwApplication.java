97488fba8a8b2fee2c5035f83b125d2c56d5375b
Gasan Djalilov <djalilov.gasan@gmail.com>
2024-08-29 23:51:38 +0500

    init

---
+++ src/main/java/uz/cbssolutions/rsocketgw/RsocketgwApplication.java
@@ -0,0 +1,13 @@
+package uz.cbssolutions.rsocketgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketgwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketgwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/cbssolutions/rsocketgw/RsocketgwApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.cbssolutions.rsocketgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketgwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
